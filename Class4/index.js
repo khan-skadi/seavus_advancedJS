f49149a364ab0da4e25a1fc434641d78cf14d022
Petre Kartalov <Petre.Kartalov@sedc.mk>
2020-02-04 18:55:34 +0100

    c4

---
+++ Class4/index.js
@@ -0,0 +1,2 @@
+// se so e zavisno od povratnata data go stavame vo callback funkcija.
+// hoisting mechanism(v8 engine). All regular functions can be called before they are defined. V8 engine reads all function definitions first. Anonymous functions must be called after they are defined.
